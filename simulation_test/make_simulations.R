03d25d9af57d6bb3e713cfe43be503165e8a7442
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-05-05 10:31:52 +0200

    update file paths

--- simulation_test/make_simulations.R
+++ simulation_test/make_simulations.R
@@ -9,27 +9,27 @@ CLI <- "no" #set to "Yes" if you want it
 brms <- "no" #set to "Yes" if you want it
 
 if(CLI == "Yes") {
-  command1 = paste0("RScript single_lambda_test.R ", lambda_strong)
-  command2 = paste0("RScript single_geography_test.R ", lambda_strong)
-  command3 = paste0("RScript dual_test.R ", lambda_strong)
+  command1 = paste0("RScript simulation_test/single_lambda_test.R ", lambda_strong)
+  command2 = paste0("RScript simulation_test/single_geography_test.R ", lambda_strong)
+  command3 = paste0("RScript simulation_test/dual_test.R ", lambda_strong)
   
   system(command1)
   system(command2)
   system(command3)
   
   ## Medium Signal
-  command4 = paste0("RScript single_lambda_test.R ", lambda_medium)
-  command5 = paste0("RScript single_geography_test.R ", lambda_medium)
-  command6 = paste0("RScript dual_test.R ", lambda_medium)
+  command4 = paste0("RScript simulation_test/single_lambda_test.R ", lambda_medium)
+  command5 = paste0("RScript simulation_test/single_geography_test.R ", lambda_medium)
+  command6 = paste0("RScript simulation_test/dual_test.R ", lambda_medium)
   
   system(command4)
   system(command5)
   system(command6)
   
   ## Weak Signal
-  command7 = paste0("RScript single_lambda_test.R ", lambda_weak)
-  command8 = paste0("RScript single_geography_test.R ", lambda_weak)
-  command9 = paste0("RScript dual_test.R ", lambda_weak)
+  command7 = paste0("RScript simulation_test/single_lambda_test.R ", lambda_weak)
+  command8 = paste0("RScript simulation_test/single_geography_test.R ", lambda_weak)
+  command9 = paste0("RScript simulation_test/dual_test.R ", lambda_weak)
   
   system(command7)
   system(command8)
